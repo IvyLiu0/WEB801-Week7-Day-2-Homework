64219635f76a030216f919b82ab0fced9c7edda4
Ying_Y <ziyingliu@Ivyyyys-MacBook-Pro.local>
2023-02-17 19:01:36 -0800

    week7day2 homework

---
+++ js/activity.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function(){
+    $("#tbody").bind("click",function(e){
+        $(e.target).closest(".tdbox").toggleClass("tdclick");
+        $('#myModal').modal('show');
+    });
+
+    $("td").click(function(){
+        var content = $(this).text();
+        var cliffName = $(".thead").eq($(this).index()).text();
+
+        if (content !="Not Avaiable"){
+            $(this).toggleClass("tdhighlight");
+
+            if ($(this).hasClass("tdhighlight")){
+                $('#myModal').css("visibility","visible");
+                $('#myModal').css("margin-top","2em");
+                $('.modal-body').append("<p>" + content + " at " + cliffName +"</p>");
+            } else {
+                $('.modal-body p:contains('+content+')').remove();
+
+                // if ($('.modal-body').has('p').length == false){
+                //     // $('#myModal').css("visibility","hidden");
+                //     // $('.modal-body').append("<h5>" + " no ativities selected " + "</h5>");
+                //     $('#myModal').css("margin-top",'0')
+                // } 
+            }
+        }
+    });
+});
